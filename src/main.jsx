7e489732bcdb6f16ad8d1cb86e9c18282466a175
Zyad Abdul-Nasser <zizo_007@live.com>
2024-04-09 14:08:56 +0200

    z

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Loading2 from './components/Loading2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Loading2 />
   </React.StrictMode>,
 )
